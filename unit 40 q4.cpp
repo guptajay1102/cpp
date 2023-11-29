721fb0a46a99f0202da53ba379f9e523dc4156ef
Jay Gupta <140606820+guptajay1102@users.noreply.github.com>
2023-11-29 23:05:06 +0530

    Create unit 40 q4.cpp

---
+++ unit 40 q4.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++>
+using namespace std;
+int main(){
+  string s= "template<typename T> class Programming language class CPlusPlus : public ProgrammingLanguge class Java : public ProgrammingLanguge
+class Phython : public ProgrammingLanguge";
+
+  string str1, str2, str3;
+  getline(cin,str1)
+  cout<<"C++ program output: "<<str1<<endl;
+
+  getline(cin,str2)
+  cout<<"Java program output: "<<str1<<endl;
+
+  getline(cin,str3)
+  cout<<"Phython program output: "<<str1<<endl;
+  return 0;
+}
